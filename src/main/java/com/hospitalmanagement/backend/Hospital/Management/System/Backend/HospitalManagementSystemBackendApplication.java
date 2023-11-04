b6251b2e4b9b24e7140a60637ba37d61d32a6bbc
Himanshu Rajauria <hrajauria1997@gmail.com>
2023-11-04 17:18:57 +0530

    Initial commit to git

---
+++ src/main/java/com/hospitalmanagement/backend/Hospital/Management/System/Backend/HospitalManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement.backend.Hospital.Management.System.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementSystemBackendApplication.class, args);
+	}
+
+}
