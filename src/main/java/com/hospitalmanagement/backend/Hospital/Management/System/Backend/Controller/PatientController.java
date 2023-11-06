632787e094d04f3441980c85bb89a02c5411d112
Himanshu Rajauria <hrajauria1997@gmail.com>
2023-11-07 00:59:41 +0530

    Developed the patient model and some features

---
+++ src/main/java/com/hospitalmanagement/backend/Hospital/Management/System/Backend/Controller/PatientController.java
@@ -0,0 +1,34 @@
+package com.hospitalmanagement.backend.Hospital.Management.System.Backend.Controller;
+
+import com.hospitalmanagement.backend.Hospital.Management.System.Backend.Models.Patient;
+import com.hospitalmanagement.backend.Hospital.Management.System.Backend.Services.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PatientController {
+
+    @Autowired
+    PatientService patientService;
+
+    @GetMapping ("/api/patient")
+    public Patient getPatientByID (@RequestParam String patientID){
+       return patientService.getPatientByID(patientID);
+    }
+
+    @PostMapping ("/api/patient/addpatient")
+    public String addPatientToDB (@RequestBody Patient obj){
+        patientService.addPatientToDB(obj);
+        return "Patient got add successfully into DB";
+    }
+
+    @DeleteMapping ("/api/patient/deletepatient")
+    public String deletePatientByID (@RequestParam String patientID){
+        patientService.dischargePatient(patientID);
+        return "Patient got discharged having patient ID - " + patientID;
+    }
+
+
+
+}
